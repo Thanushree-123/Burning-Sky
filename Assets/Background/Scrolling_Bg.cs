d215b51bb9e9f12b0e3d2c4deeec6d3e0ee53772
Thanushree-123 <thanushree196@gmail.com>
2021-11-29 11:59:18 +0530

    Moving background and player

---
+++ Assets/Background/Scrolling_Bg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling_Bg : MonoBehaviour
+{
+    public float speed = -2f;
+
+void Update ()
+ {
+   transform.Translate(0f, speed * Time.deltaTime, 0f);
+   if(transform.position.y <= -20)
+   {
+     transform.Translate(0f, 40f, 0f);
+   }
+ }
+}
